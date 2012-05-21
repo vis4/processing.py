761554dd90db2e8687895a4c8d3aa41a2a1903e7
Gregor Aisch <contact@vis4.net>
2012-05-21 10:40:01 +0200

    ..

--- sketch/sketch.py
+++ sketch/sketch.py
@@ -1,22 +1,30 @@
 
 from p9 import *
-import csv
-
-cities = []
 
 
 def setup():
-    global cities
-    size(360, 180, JAVA2D)
-    db = csv.reader(open('../cities1000.txt', 'r'), dialect='excel-tab')
-    for city in db:
-        cities.append(map(float, city[4:6]))
-    noLoop()
+    size(400, 400)
+    stroke(255)
+    background(192, 64, 0)
+    noFill()
+    smooth()
+
+pts = []
 
 
 def draw():
-    background(0)
-    stroke(255)
-    print "Processing w/o Java syntax"
-    for lat, lon in cities:
-        point(lon + 180, 90 - lat)
+    curveTightness(0.03)
+    background(192, 64, 0)
+    beginShape()
+    for x, y in pts:
+        curveVertex(x, y)
+    endShape()
+
+
+def mouseDragged(evt):
+    if len(pts) > 0:
+        x, y = pts[-1]
+        d = dist(x, y, mouse.x, mouse.y)
+        if d < 30:
+            return
+    pts.append((mouse.x, mouse.y))
